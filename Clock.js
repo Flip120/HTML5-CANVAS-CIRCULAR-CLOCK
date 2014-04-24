c74d9e04f58d02837030ec4084be66f97fe4c73c
isaax2 <isaacisaacisaac2@gmail.com>
2014-04-23 23:25:25 -0500

    New method for clear clock interval.
    The code was formatted too.

--- Clock.js
+++ Clock.js
@@ -1,182 +1,188 @@
-function Clock( params ){
+function Clock(params) {
 
-	//Config
-	var doStep       = {};
-	var doDraw		= {};
-	var type         = params.type || Clock.TYPE_CLOCKWISE_INCREMENT;
-	var initValue    = params.initValue || (type <= Clock.TYPE_CLOCKWISE_DECREMENT)?0:2;
-	var data         = initValue;
-	var time         = parseInt((params.time)? (params.time) : 60);
-	var showText     = (params.showText != null)? (params.showText) : true;
-	var textColor    = (showText)? (params.textColor || 'white') : null;
-	var bodyColor    = (params.bodyColor)?params.bodyColor : '#FF0000';
-	var borderColor  = (params.borderColor)?params.borderColor : 'transparent';
-	var bodyWidth    = params.bodyWidth || 10;
-	var borderWidth  = params.borderWidth || bodyWidth;
-	var bodySeparation   = (params.bodySeparation)?(params.bodySeparation) : 0;
-	var fontFace     = (params.fontFace)? params.fontFace:'Arial';
-	var endCallback  = params.endCallBack || function(){console.log('TIME OUT!!')};
-	var dateStart    = new Date();
-	var freq         = (params.freq != null) ? params.freq : 10;
-	var netRadius    = Math.max(bodyWidth,borderWidth);
-	var autoDraw     = (params.autoDraw != null)? params.autoDraw : true;
+    //Config
+    var doStep = {};
+    var doDraw = {};
+    var type = params.type || Clock.TYPE_CLOCKWISE_INCREMENT;
+    var initValue = params.initValue || (type <= Clock.TYPE_CLOCKWISE_DECREMENT) ? 0 : 2;
+    var data = initValue;
+    var time = parseInt((params.time) ? (params.time) : 60);
+    var showText = (params.showText != null) ? (params.showText) : true;
+    var textColor = (showText) ? (params.textColor || 'white') : null;
+    var bodyColor = (params.bodyColor) ? params.bodyColor : '#FF0000';
+    var borderColor = (params.borderColor) ? params.borderColor : 'transparent';
+    var bodyWidth = params.bodyWidth || 10;
+    var borderWidth = params.borderWidth || bodyWidth;
+    var bodySeparation = (params.bodySeparation) ? (params.bodySeparation) : 0;
+    var fontFace = (params.fontFace) ? params.fontFace : 'Arial';
+    var endCallback = params.endCallBack || function() {
+        console.log('TIME OUT!!')
+    };
+    var dateStart = new Date();
+    var freq = (params.freq != null) ? params.freq : 10;
+    var netRadius = Math.max(bodyWidth, borderWidth);
+    var autoDraw = (params.autoDraw != null) ? params.autoDraw : true;
 
-	var clockInterval,
-		   drawInterval,
-		   diff;
+    var clockInterval,
+            drawInterval,
+            diff;
 
-		//Draw Funcs
-		doStep[Clock.TYPE_CLOCKWISE_INCREMENT] = function(Clock){
+    //Draw Funcs
+    doStep[Clock.TYPE_CLOCKWISE_INCREMENT] = function(Clock) {
 
-	   if(data >= 2){
-	       clearInterval(clockInterval);
-	       data = 2;
-	       endCallback();
-	       return;
-	   }
+        if (data >= 2) {
+            clearInterval(clockInterval);
+            data = 2;
+            endCallback();
+            return;
+        }
 
-	   var now	= new Date();
-	   diff		= ((now.getTime() - dateStart.getTime())/1000);
-	   data 	= diff * 2 / time;
-	};
+        var now = new Date();
+        diff = ((now.getTime() - dateStart.getTime()) / 1000);
+        data = diff * 2 / time;
+    };
 
-		doDraw[Clock.TYPE_CLOCKWISE_INCREMENT] = function(Clock){
-		eraseZone(Clock);
-	   	drawBorder(Clock);
-		Clock.ctx.beginPath();
-	   	Clock.ctx.strokeStyle = bodyColor;
-	   	Clock.ctx.lineWidth   = bodyWidth;
-	   	Clock.ctx.arc(Clock.x, Clock.y, Clock.radius, initValue*Math.PI, data*Math.PI, false);
-	   	Clock.ctx.stroke();
-	   	drawText(Clock);
-	}
+    doDraw[Clock.TYPE_CLOCKWISE_INCREMENT] = function(Clock) {
+        eraseZone(Clock);
+        drawBorder(Clock);
+        Clock.ctx.beginPath();
+        Clock.ctx.strokeStyle = bodyColor;
+        Clock.ctx.lineWidth = bodyWidth;
+        Clock.ctx.arc(Clock.x, Clock.y, Clock.radius, initValue * Math.PI, data * Math.PI, false);
+        Clock.ctx.stroke();
+        drawText(Clock);
+    }
 
-	doStep[Clock.TYPE_CLOCKWISE_DECREMENT] = function(Clock){
+    doStep[Clock.TYPE_CLOCKWISE_DECREMENT] = function(Clock) {
 
-	   if(data >= 2){
-	       clearInterval(clockInterval);
-	       data = 0;
-	       endCallback();
-	       return;
-	   }
+        if (data >= 2) {
+            clearInterval(clockInterval);
+            data = 0;
+            endCallback();
+            return;
+        }
 
-	   var now	= new Date();
-	   diff     = ((now.getTime() - dateStart.getTime())/1000);
-	   data 	= diff * 2 / time;
-	};
+        var now = new Date();
+        diff = ((now.getTime() - dateStart.getTime()) / 1000);
+        data = diff * 2 / time;
+    };
 
-	doDraw[Clock.TYPE_CLOCKWISE_DECREMENT] = function(Clock){
-			eraseZone(Clock);
-	   	drawBorder(Clock);
-			Clock.ctx.beginPath();
-	   	Clock.ctx.strokeStyle = bodyColor;
-	   	Clock.ctx.lineWidth   = bodyWidth;
-	   	Clock.ctx.arc(Clock.x, Clock.y, Clock.radius, data*Math.PI,initValue*Math.PI, false);
-	   	Clock.ctx.stroke();
-	   	drawText(Clock);
-	}
+    doDraw[Clock.TYPE_CLOCKWISE_DECREMENT] = function(Clock) {
+        eraseZone(Clock);
+        drawBorder(Clock);
+        Clock.ctx.beginPath();
+        Clock.ctx.strokeStyle = bodyColor;
+        Clock.ctx.lineWidth = bodyWidth;
+        Clock.ctx.arc(Clock.x, Clock.y, Clock.radius, data * Math.PI, initValue * Math.PI, false);
+        Clock.ctx.stroke();
+        drawText(Clock);
+    }
 
-	doStep[Clock.TYPE_COUNTERCLOCKWISE_INCREMENT] = function(Clock){
-	   if(data <=0 ){
-	       clearInterval(clockInterval);
-	       data = 2;
-	       endCallback();
-	       return;
-	   }
+    doStep[Clock.TYPE_COUNTERCLOCKWISE_INCREMENT] = function(Clock) {
+        if (data <= 0) {
+            clearInterval(clockInterval);
+            data = 2;
+            endCallback();
+            return;
+        }
 
-	   	var now		= new Date();
-	 	diff        = ((now.getTime() - dateStart.getTime())/1000);
-	   	data 		= 2 - diff * 2 / time;
-	};
+        var now = new Date();
+        diff = ((now.getTime() - dateStart.getTime()) / 1000);
+        data = 2 - diff * 2 / time;
+    };
 
-	doDraw[Clock.TYPE_COUNTERCLOCKWISE_INCREMENT] = function(Clock){
-			eraseZone(Clock);
-	   	drawBorder(Clock);
-	   	Clock.ctx.beginPath();
-	   	Clock.ctx.strokeStyle = bodyColor;
-	   	Clock.ctx.lineWidth   = bodyWidth;
-	   	Clock.ctx.arc(Clock.x, Clock.y, Clock.radius, data*Math.PI, 0*Math.PI, false);
-	   	Clock.ctx.stroke();
-	   	drawText(Clock);
-	}
+    doDraw[Clock.TYPE_COUNTERCLOCKWISE_INCREMENT] = function(Clock) {
+        eraseZone(Clock);
+        drawBorder(Clock);
+        Clock.ctx.beginPath();
+        Clock.ctx.strokeStyle = bodyColor;
+        Clock.ctx.lineWidth = bodyWidth;
+        Clock.ctx.arc(Clock.x, Clock.y, Clock.radius, data * Math.PI, 0 * Math.PI, false);
+        Clock.ctx.stroke();
+        drawText(Clock);
+    }
 
 
-	doStep[Clock.TYPE_COUNTERCLOCKWISE_DECREMENT] = function(Clock){
-	   
-	   if(data <= 0){
-	       clearInterval(clockInterval);
-	       data = 0;
-	       endCallback();
-	       return;
-	   }
-	   	
-	   	var now	= new Date();
-	 	diff	= ((now.getTime() - dateStart.getTime())/1000);
-	   	data 	= 2 - diff * 2 / time;
-	};
+    doStep[Clock.TYPE_COUNTERCLOCKWISE_DECREMENT] = function(Clock) {
 
-		doDraw[Clock.TYPE_COUNTERCLOCKWISE_DECREMENT] = function(Clock){
-			eraseZone(Clock);
-	   	drawBorder(Clock);
-	   	Clock.ctx.beginPath();
-	   	Clock.ctx.strokeStyle = bodyColor;
-	   	Clock.ctx.lineWidth   = bodyWidth;
-	   	Clock.ctx.arc(Clock.x, Clock.y, Clock.radius, 0*Math.PI, data*Math.PI, false);
-	   	Clock.ctx.stroke();
-	   	drawText(Clock);
-		}
+        if (data <= 0) {
+            clearInterval(clockInterval);
+            data = 0;
+            endCallback();
+            return;
+        }
 
-	function drawBorder(Clock){
-	   Clock.ctx.beginPath();
-	   Clock.ctx.strokeStyle = borderColor;
-	   Clock.ctx.lineWidth      = borderWidth;
-	   Clock.ctx.arc(Clock.x, Clock.y, Clock.radius+bodySeparation, 0*Math.PI, 2*Math.PI, false);
-	   Clock.ctx.stroke();
-	}
+        var now = new Date();
+        diff = ((now.getTime() - dateStart.getTime()) / 1000);
+        data = 2 - diff * 2 / time;
+    };
 
-	function drawText(Clock){
+    doDraw[Clock.TYPE_COUNTERCLOCKWISE_DECREMENT] = function(Clock) {
+        eraseZone(Clock);
+        drawBorder(Clock);
+        Clock.ctx.beginPath();
+        Clock.ctx.strokeStyle = bodyColor;
+        Clock.ctx.lineWidth = bodyWidth;
+        Clock.ctx.arc(Clock.x, Clock.y, Clock.radius, 0 * Math.PI, data * Math.PI, false);
+        Clock.ctx.stroke();
+        drawText(Clock);
+    }
 
-			if(!showText){
-				return;
-			}
+    function drawBorder(Clock) {
+        Clock.ctx.beginPath();
+        Clock.ctx.strokeStyle = borderColor;
+        Clock.ctx.lineWidth = borderWidth;
+        Clock.ctx.arc(Clock.x, Clock.y, Clock.radius + bodySeparation, 0 * Math.PI, 2 * Math.PI, false);
+        Clock.ctx.stroke();
+    }
 
-	 	Clock.ctx.fillStyle     = textColor;
-		Clock.ctx.font             = "bold "+(Clock.radius/1.5)+"px "+fontFace;
-	    Clock.ctx.textAlign     = 'center';
-	    Clock.ctx.fillText( Math.ceil(time - diff), Clock.x, Clock.y +(Clock.radius/4));
-	    Clock.ctx.restore();
-	}
+    function drawText(Clock) {
 
-		function eraseZone(Clock){
-		Clock.ctx.clearRect(Clock.x - Clock.radius - netRadius, Clock.y - Clock.radius - netRadius, Clock.radius * 2 + netRadius*2, Clock.radius * 2 + netRadius*2);
-		}
+        if (!showText) {
+            return;
+        }
 
-	//PUBLIC:
-	this.ctx       = params.context;
-	this.x         = params.x || 300,
-	this.y         = params.y || 300,
-	this.radius    = params.radius || 200;
-	this.step      = function(){
-	   clockInterval = setInterval(function(Clock){
-	       doStep[type](Clock);
-	   },freq, this);
+        Clock.ctx.fillStyle = textColor;
+        Clock.ctx.font = "bold " + (Clock.radius / 1.5) + "px " + fontFace;
+        Clock.ctx.textAlign = 'center';
+        Clock.ctx.fillText(Math.ceil(time - diff), Clock.x, Clock.y + (Clock.radius / 4));
+        Clock.ctx.restore();
+    }
 
-	   if(autoDraw){
-	   		drawInterval = setInterval(function(Clock){
-		           Clock.draw();
-		    },freq, this);
-	   }
-	};
-	this.draw = function(){
-	   this.ctx.save();
-	   doDraw[type](this);
-	   this.ctx.restore();
-	}
-	this.setEndCallBack = function(newCallBack){
-		endCallback = newCallBack;
-	}
+    function eraseZone(Clock) {
+        Clock.ctx.clearRect(Clock.x - Clock.radius - netRadius, Clock.y - Clock.radius - netRadius, Clock.radius * 2 + netRadius * 2, Clock.radius * 2 + netRadius * 2);
+    }
+
+    //PUBLIC:
+    this.ctx = params.context;
+    this.x = params.x || 300,
+            this.y = params.y || 300,
+            this.radius = params.radius || 200;
+    this.step = function() {
+        clockInterval = setInterval(function(Clock) {
+            doStep[type](Clock);
+        }, freq, this);
+
+        if (autoDraw) {
+            drawInterval = setInterval(function(Clock) {
+                Clock.draw();
+            }, freq, this);
+        }
+    };
+    this.draw = function() {
+        this.ctx.save();
+        doDraw[type](this);
+        this.ctx.restore();
+    };
+    this.setEndCallBack = function(newCallBack) {
+        endCallback = newCallBack;
+    };
+    //clear the interval
+    this.stopEndCallBack = function(){
+        clearInterval(clockInterval);
+    }; 
 }
-	Clock.TYPE_CLOCKWISE_INCREMENT           = 0;
-	Clock.TYPE_CLOCKWISE_DECREMENT           = 1;
-	Clock.TYPE_COUNTERCLOCKWISE_INCREMENT = 2;
-	Clock.TYPE_COUNTERCLOCKWISE_DECREMENT = 3;
+Clock.TYPE_CLOCKWISE_INCREMENT = 0;
+Clock.TYPE_CLOCKWISE_DECREMENT = 1;
+Clock.TYPE_COUNTERCLOCKWISE_INCREMENT = 2;
+Clock.TYPE_COUNTERCLOCKWISE_DECREMENT = 3;
